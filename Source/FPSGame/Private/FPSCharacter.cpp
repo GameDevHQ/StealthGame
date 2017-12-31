df48a0c867e496c6d545c3f7916471c29b0d1811
Valeryi Savich <relrin78@gmail.com>
2018-01-01 01:25:27 +0300

    Implemented Objective Actor for a game

--- Source/FPSGame/Private/FPSCharacter.cpp
+++ Source/FPSGame/Private/FPSCharacter.cpp
@@ -8,7 +8,8 @@
 #include "Kismet/GameplayStatics.h"
 
 
-AFPSCharacter::AFPSCharacter()
+AFPSCharacter::AFPSCharacter():
+bIsCarryingObjective(false)
 {
 	// Create a CameraComponent	
 	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
