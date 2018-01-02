e70717942f767cd37608334cb427893f34db041d
Valeryi Savich <relrin78@gmail.com>
2018-01-02 14:15:03 +0300

    Implemented Extraction Zone

---
+++ Source/FPSGame/Private/FPSExtractionZone.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FPSExtractionZone.h"
+#include "Components/BoxComponent.h"
+#include "Components/DecalComponent.h"
+
+// Sets default values
+AFPSExtractionZone::AFPSExtractionZone()
+{
+    OverlapComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("OverlapComponent"));
+    OverlapComponent->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+    OverlapComponent->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+    OverlapComponent->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+    OverlapComponent->SetBoxExtent(FVector(200.f));
+    RootComponent = OverlapComponent;
+
+    OverlapComponent->OnComponentBeginOverlap.AddDynamic(this, &AFPSExtractionZone::HandleOverlap);
+
+    DecalComponent = CreateDefaultSubobject<UDecalComponent>(TEXT("DecalComponent"));
+    DecalComponent->DecalSize = FVector(200.f, 200.f, 200.f);
+    DecalComponent->SetupAttachment(OverlapComponent);
+}
+
+void AFPSExtractionZone::HandleOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, 
+                                       UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, 
+                                       bool bFromSweep, const FHitResult& SweepResult)
+{
+
+}
